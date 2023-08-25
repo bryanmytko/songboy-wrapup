4c46cc9313d198cdfaecac8e88f351c10c55db31
Bryan Mytko <bryanmytko@gmail.com>
2023-08-25 00:36:40 -0400

    Update to Next13; migrate pages -> app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Songboy Wrapup</h1>
+      </main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
